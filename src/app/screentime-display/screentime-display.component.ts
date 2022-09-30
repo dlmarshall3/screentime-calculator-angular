75cd995651f43018924bd5c44ca913372d70b967
david-carmigo <david.marshall@carmigo.io>
2022-09-29 19:28:26 -0500

    added three years

--- src/app/screentime-display/screentime-display.component.ts
+++ src/app/screentime-display/screentime-display.component.ts
@@ -9,6 +9,7 @@ export class ScreentimeDisplayComponent implements OnChanges {
     weeklyScreentime: number = 0;
     monthlyScreentime: number = 0;
     yearlyScreentime: number = 0;
+    threeYearScreenTime: number = 0;
     fiveYearScreentime: number = 0;
     tenYearScreentime: number = 0;
     twentyfiveYearScreentime: number = 0;
@@ -24,6 +25,7 @@ export class ScreentimeDisplayComponent implements OnChanges {
     this.weeklyScreentime = this.dailyScreentime * 7;
     this.monthlyScreentime = this.dailyScreentime * 30;
     this.yearlyScreentime = this.dailyScreentime * 365;
+    this.threeYearScreenTime = this.yearlyScreentime * 3;
     this.fiveYearScreentime = this.yearlyScreentime * 5;
     this.tenYearScreentime = this.yearlyScreentime * 10;
     this.twentyfiveYearScreentime = this.yearlyScreentime * 25;
